2b189cb7ae6b92a170d4b950c07e731431f8d1c2
Cyma-s <ssol0319@naver.com>
2022-11-17 15:05:42 +0900

    feat : add api that finds medicine name from medicine images

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from model.AI_model import AI
 
 app = Flask(__name__)
 
@@ -8,5 +10,23 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/ai/medicine', methods=['POST'])
+def getMedicineName():
+    if request.method == 'POST':
+        directory_path = 'images/'
+        medicine_images = request.files.getlist('files')
+        merged_file_name = medicine_images[0].filename
+        image_name = []
+        for image in medicine_images:
+            file_name = secure_filename(directory_path + image.filename)
+            image_name.append(file_name)
+            image.save(file_name)
+        print(image_name)
+        ai_model = AI()
+        ai_model.combination(image_name[0], image_name[1], merged_file_name)
+        result = ai_model.test(merged_file_name + ".merged_image.jpg")
+        return {'result' : "%s" % result}
+
+
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
